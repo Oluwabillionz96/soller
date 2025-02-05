fd62b7db682661d2c93d5855cf60d8418c9afdf5
oluwabillionz96 <goodluckreuben96@gmail.com>
2025-02-05 17:15:45 +0100

    update

--- src/Componenets/Solutions/Section3.jsx
+++ src/Componenets/Solutions/Section3.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Quote } from "../Navbar/NavBar";
+import computer from "../../assets/computer.png";
+import "./section3.css";
 
 const Section3 = () => {
-  return <section>Section3</section>;
+  return (
+    <section id="conclusion">
+      <p className="colored">Get the Sun to power your home</p>
+      <h2>All the power that you need for your house is now available</h2>
+      <div className="abs">
+        <Quote />
+        <p>Egestas fringilla aliquam leo</p>
+      </div>
+      <img src={computer} alt="computer image" className="conclusion-img" />
+      <div className="circles circle1 desktop"></div>
+      <div className="circles circle2 "></div>
+      <div className="circles circle3 desktop"></div>
+    </section>
+  );
 };
 
 export default Section3;
